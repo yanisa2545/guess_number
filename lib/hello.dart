776c246fa146499bd116469f7351726fd8570a48
yanisa2545 <109447292+yanisa2545@users.noreply.github.com>
2022-08-13 21:16:27 +0700

    Update hello.dart

--- lib/hello.dart
+++ lib/hello.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 import 'package:guess_number/game.dart';
 void main() {
   var game = Game();
-  int result = 0;
+  int re1 = 0;
+  List<int> myList = [];
+  int fail = 1;
   do {
     stdout.write('Please guess the number between 1 and 100 : '); //UX = User Experience
     var input = stdin.readLineSync();
@@ -11,19 +13,21 @@ void main() {
       print('Please enter number only');
       continue;
     }
-    result = game.doGuess(guess);
-    if (result == 3) {
-      stdout.write('Game Over : ');
+    re1 = game.doGuess(guess);
+    if (re1 == 0) {
+      fail++;
     }
-  } while (result != 1);
-    int re = 0;
+  } while (re1 != 1);
+    myList.add(fail);
+    int re2 = 0;
     var t = 0;
     do {
       print('เล่นต่อ yes / ไม่เล่นต่อ no');
       stdout.write('Do you keep playing : ');
       var over = stdin.readLineSync();
       if (over == 'yes') {
-        var g = Game();
+        var g2 = Game(maxRandom: 1000);
+        int fail1 = 1;
         do {
           stdout.write('Please guess the number between 1 and 100 : ');
           var input = stdin.readLineSync();
@@ -32,13 +36,55 @@ void main() {
             print('Please enter number only');
             continue;
           }
-          re = g.doGuess(guess1);
+          re2= g2.doGuess(guess1);
+          if (re2 == 0) {
+            fail1++;
+          }
         }
-        while(re != 1);
+        while(re2 != 1);
+        myList.add(fail1);
         }
-      else{t = 1;
+      else if(over == 'no') {
+        t = 1;
+      }
+      else{
+        t = 2;
+        stdout.write('Do you keep playing : ');
+        var over = stdin.readLineSync();
+        if (over == 'yes') {
+          t = 0;
+          var g2 = Game(maxRandom: 1000);
+          do {
+            stdout.write('Please guess the number between 1 and 100 : ');
+            var input = stdin.readLineSync();
+            var guess1 = int.tryParse(input!);
+            if (guess1 == null) {
+              print('Please enter number only');
+              continue;
+            }
+            re2 = g2.doGuess(guess1);
+
+          }
+          while(re2 != 1);
+
+        }
+
+        else if(over == 'no') {
+          t = 1;
+        }
+        else{
+          t = 2;
+        }
+        while(t == 0);
       }
         }
     while(t == 0);
+    var all = myList.length;
+      print('you are play $all game');
+      for(int i=0; i<myList.length; i++){
+        print('Game #${i+1} :  '+'$all Guesses');
+    }
         }
+
+
 
